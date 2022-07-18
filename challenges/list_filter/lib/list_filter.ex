3a3654a6937db5fd03c90d5cb33b292edc02c39c
Samuel <sampaioxsamuel@gmail.com>
2022-07-17 21:53:57 -0300

    enums

---
+++ challenges/list_filter/lib/list_filter.ex
@@ -0,0 +1,27 @@
+defmodule ListFilter do
+  require Integer
+
+  def call([]), do: 0
+  def call(list), do: count_odd_numbers(list)
+
+  defp is_number_valid(string) do
+    if Integer.parse(string) != :error do
+      string
+    end
+  end
+
+  defp remove_non_numbers(list) do
+    Enum.filter(list, fn element -> is_number_valid(element) end)
+  end
+
+  defp list_string_to_number(list) do
+    Enum.map(list, fn x -> String.to_integer(x) end)
+  end
+
+  defp count_odd_numbers(list) do
+    remove_strings = remove_non_numbers(list)
+    numbers = list_string_to_number(remove_strings)
+
+    Enum.count(numbers, fn element -> Integer.is_odd(element) end)
+  end
+end
---
+++ challenges/list_filter/test/list_filter_test.exs
@@ -0,0 +1,13 @@
+defmodule ListFilterTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "return correct number of odd numbers" do
+      list1 = ["1", "3", "6", "43", "banana", "6", "abc"]
+      list2 = ["3", "banana", "6", "abacaxi"]
+
+      assert ListFilter.call(list1) == 3
+      assert ListFilter.call(list2) == 1
+    end
+  end
+end
